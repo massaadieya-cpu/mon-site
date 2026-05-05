af0aec40064dd61c289df750b10717bea6495b72
massaadieya-cpu <massaadieya@gmail.com>
2026-05-05 14:28:50 +0100

    fix ssl pdo

--- config.php
+++ config.php
@@ -1,13 +1,11 @@
 <?php
 if (getenv('MYSQLHOST')) {
-    // En ligne sur Railway
     $host = 'trolley.proxy.rlwy.net';
     $port = '12388';
     $db   = 'railway';
     $user = 'root';
     $pass = 'dXRLbawXJqRwzuRyciEhabPhIbuzbUsP';
 } else {
-    // En local sur XAMPP
     $host = 'localhost';
     $port = '3306';
     $db   = 'rh_altutex';
@@ -19,7 +17,8 @@ $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 try {
      $pdo = new PDO($dsn, $user, $pass, [
          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+         PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
      ]);
 } catch (\PDOException $e) {
      die("Erreur de connexion : " . $e->getMessage());
