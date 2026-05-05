5cc09d7e4ae65a057b41d15047534b0df02cb218
massaadieya-cpu <massaadieya@gmail.com>
2026-05-05 01:59:37 +0100

    fix config mysql railway

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
-// config.php
-$host = 'localhost';
-$db   = 'rh_altutex'; // <--- CHANGE CECI (ex: 'altutex_db')
-$user = 'root';
-$pass = '';
+$host = getenv('MYSQLHOST');
+$db   = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 $charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 try {
      $pdo = new PDO($dsn, $user, $pass, [
          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
